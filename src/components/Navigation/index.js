d5df3dd65984226476814ee4d99ba5835e0c912e
dună <alexdunarentu@gmail.com>
2022-04-02 14:44:02 +0300

    First commit

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const Navigation = () => {
+  return (
+    <nav className="Navigation">
+      <p className="NavigationAction">Sign Out</p>
+    </nav>
+  );
+};
+
+export default Navigation;
